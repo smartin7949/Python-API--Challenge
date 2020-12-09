cbc9a2c8acc16ec40b22bc2a06b2ac4aa94dfa54
smartin7949 <smartin7949@gmail.com>
2020-12-08 20:08:51 -0600

    update hw

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1a3ea81d34a5f39d1e08c6b301c6eb31"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCvP0AQxlWTP3mhiKgVm9x_1AtChqs3AaI"
